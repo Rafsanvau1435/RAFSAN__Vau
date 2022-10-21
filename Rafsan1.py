93c34a2c7f28f9a19e2a9449db59022c70f55111
Rafsanvau1435 <116366345+Rafsanvau1435@users.noreply.github.com>
2022-10-21 22:54:13 +0800

    Create Rafsan1.py

---
+++ Rafsan1.py
@@ -0,0 +1,50 @@
+2004 Years old id crack free command
+
+       
+
+                   🎑Enjoy🎑
+                   
+
+⚽ apt update && apt upgrade
+
+
+⚽ apt install python
+
+
+⚽ apt install python2
+
+
+⚽ pip install mechanize lolcat
+
+
+⚽ pip install requests bs4
+
+
+⚽ pip2 install requests bs4
+
+
+⚽ pip install futures
+
+
+⚽ apt install git
+
+
+⚽ rm -rf  Rafsan2004-Free
+
+
+⚽  git clone https://github.com/Rafsan_vau/Rafsan2004-Free.git
+
+
+⚽ cd Rafsan2004-Free
+
+
+⚽ python Free➳ᴹᴿ᭄Rafsan.py
+
+
+JOIN MY WHATSAPP GROUP : https://chat.whatsapp.com/DJ0SZudWurs9mPhSIMvEYm
+
+
+Thanks for use This Tools ⚾
+
+
+
